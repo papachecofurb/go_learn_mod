189e28876b114999a0b9d3c2855db3452f86fb87
papacheco <papachecofurb@gmail.com>
2024-06-13 10:26:25 -0300

    mod hello

---
+++ hello/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/papachecofurb/go_learn_mod/greetings"
+)
+
+func main() {
+	// Get a greeting message and print it.
+	message := greetings.Hello("Gladys")
+	fmt.Println(message)
+}
